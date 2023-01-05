cf97159cddce7e863a39d7da574857e549661eda
soyoung <0001401@naver.com>
2023-01-05 18:19:37 +0900

    [0105] redirects url

--- pages/component/NavBar.js
+++ pages/component/NavBar.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <div>
+      {/* 브라우저 tab에 보여질 내용 설정하기 (여기에 쓰면 전체) */}
+      {/* <Head>
+        <title>soyoung | project</title>
+      </Head> */}
       <Link href="/">
         <span>Home</span>
       </Link>
